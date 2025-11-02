001c256f2d349f3205a364faec9956afc090460a
Killerulya2015 <Killerulya2015@yandex.ru>
2025-11-02 11:00:18 +0000

    start project issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,3 @@
 # Применяем миграции базы данных
 echo "Applying database migrations..."
 python manage.py migrate
-
-# Запускаем команду, переданную в Docker CMD.
-# `exec "$@"` заменяет текущий процесс на новый,
-# что является лучшей практикой для Docker-контейнеров.
-exec "$@"
